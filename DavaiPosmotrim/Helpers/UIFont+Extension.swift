1083840b3e438e2ebea36ba2c8e132b8e4aa2fc3
Eldar Aidumov <superaydumov@me.com>
2024-04-20 15:23:25 +0300

    [DP-1] - Добавил кастомные шрифты в проект. Создал расширение UIFont+Extension для использования шрифтов. Изменил название ассета, чтобы исключить пересечение с названием системного цвета. Упорядочил файлы в проекте.

---
+++ DavaiPosmotrim/Helpers/UIFont+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIFont+Extension.swift
+//  DavaiPosmotrim
+//
+//  Created by Эльдар Айдумов on 20.04.2024.
+//
+
+import UIKit
+
+extension UIFont {
+
+    static let textHeadingFont = UIFont(name: "DaysOne-Regular", size: 24)
+    static let textLabelFont = UIFont(name: "DaysOne-Regular", size: 16)
+
+    static let textParagraphRegularFont = UIFont(name: "Inter-Regular", size: 14)
+    static let textParagraphBoldFont = UIFont(name: "Inter-Bold", size: 14)
+    static let textCaptionRegularFont = UIFont(name: "Inter-Regular", size: 12)
+    static let textButtonMediumFont = UIFont(name: "Inter-Medium", size: 14)
+    static let textButtonBoldFont = UIFont(name: "Inter-SemiBold", size: 14)
+
+}
