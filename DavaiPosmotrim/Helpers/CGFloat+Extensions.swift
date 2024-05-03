f6b19c5cb46ababbbc17e903dc84cf36c624a9e9
Sergey Kemenov <skemenov@icloud.com>
2024-05-03 10:04:02 +0300

    Добавил расширение для CGFloat, внес константы для 3х `cornerRadius`

---
+++ DavaiPosmotrim/Helpers/CGFloat+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  CGFloat+Extensions.swift
+//  DavaiPosmotrim
+//
+//  Created by Sergey Kemenov on 03.05.2024.
+//
+
+import Foundation
+
+extension CGFloat {
+    /// CornerRadius/Base = 12px
+    static let radiusBase = 12.0
+
+    /// CornerRadius/Medium = 16px
+    static let radiusMedium = 16.0
+
+    /// CornerRadius/Large = 24px
+    static let radiusLarge = 24.0
+}
