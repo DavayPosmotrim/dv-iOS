e6d603b1d4eebcdaaca60ba2edf83fddc80b701b
Sergey Kemenov <skemenov@icloud.com>
2024-05-05 14:42:11 +0300

    Added `NoInternetViewController` into `Mistakes` folder

---
+++ DavaiPosmotrim/Mistakes/NoInternetViewController.swift
@@ -0,0 +1,12 @@
+//
+//  NoInternetViewController.swift
+//  DavaiPosmotrim
+//
+//  Created by Sergey Kemenov on 03.05.2024.
+//
+
+import UIKit
+
+final class NoInternetViewController: UIViewController {
+    
+}
