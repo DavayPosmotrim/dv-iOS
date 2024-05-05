d04106255a03dd522db617b2ee0be246a2eaee1e
Sergey Kemenov <skemenov@icloud.com>
2024-05-05 21:28:06 +0300

    Добавил статичные переменные для отступов, высоты строк, кнопки, плейсхолдера

--- DavaiPosmotrim/Helpers/CGFloat+Extensions.swift
+++ DavaiPosmotrim/Helpers/CGFloat+Extensions.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 extension CGFloat {
+    // MARK: - CornerRadius
+
     /// CornerRadius/Base = 12px
     static let radiusBase = 12.0
 
@@ -16,4 +18,45 @@ extension CGFloat {
 
     /// CornerRadius/Large = 24px
     static let radiusLarge = 24.0
+
+    // MARK: - Spacing
+
+    /// Spacing/Base = 12px
+    static let spacingBase = 12.0
+
+    /// Spacing/Medium = 16px
+    static let spacingMedium = 16.0
+
+    /// Spacing/Extra Large = 32px
+    static let spacingExtraLarge = 32.0
+
+    // MARK: - TextHeight
+
+    /// Text/Heading = 28px
+    static let textHeadingHeight = 28.0
+
+    /// Text/Label = 24px
+    static let textLabelHeight = 24.0
+
+    /// Text/Title = 40px
+    static let textTitleHeight = 40.0
+
+    /// Text/Paragraph = 20px
+    static let textParagraphHeightOneLine = 20.0
+
+    /// Text/Paragraph two lines = 40px
+    static let textParagraphHeightTwoLines = 40.0
+
+    /// Text/Caption = 16px
+    static let textCaptionHeight = 16.0
+
+    // MARK: - Button
+
+    /// Button height = 48px
+    static let buttonHeight = 48.0
+
+    // MARK: - Placeholder
+
+    /// Placeholder size (square) = 200px
+    static let placeholderSize = 200.0
 }
